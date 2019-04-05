8cc7fdbe4bdf03f73a5d9426d454468e9580c4ff
Unknown <massive.annas@gmail.com>
2019-04-05 17:26:02 +0700

    Added Nomor 1

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -4,21 +4,29 @@
 
 void* factorial(void *arg);
 
-int main(){
-	int err;
+int main(int argc, char *argv[]){
+	int err, i;
     pthread_t tid[100];
 
-    //scan arg
+    //sort
+    for (i = 1; i <= argc; i++){
+    	if(array[i]>array[i+1])
+	{
+    		temp = array[i];
+    		array[i] = array[i+1];
+    		array[i+1] = temp;
+	}
+    }//sort done;
 
-	while(1){
-		err = pthread_create( &(tid[i]), NULL, prime, (void*) (arr[i]));
+	for(i=0;i<argc;i++){
+		err = pthread_create( &(tid[i]), NULL, prime, (void*) (argv[i]));
 		if(err){
             fprintf(stderr,"Error - pthread_create() return code: %d\n",err);
             exit(EXIT_FAILURE);
         }
 	}
 
-	for(i=0;i<T;i++){
+	for(i=0;i<argc;i++){
 		pthread_join(tid[i], NULL);
 	}
 
